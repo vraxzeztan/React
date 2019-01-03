4371244292d4e069ccb8cc922ffd8d100ad70116
vraxzeztan <anusarsoni88@gmail.com>
2019-01-03 16:53:50 +0530

    added multicomponent

---
+++ public/welcome7.js
@@ -0,0 +1,35 @@
+//// Multi Component
+
+var PagePic = React.createClass({
+    render : function(){
+        return (
+            <img src={'https://graph.facebook.com' + this.props.pagename + '/picture'} />
+        )
+    }
+});
+
+var PageLink = React.createClass({
+    render : function(){
+        return (
+            <a href={'https://www.facebook.com/+ this.props.pagename'}>
+            {this.props.pagename}
+            </a>
+        )
+    }
+});
+
+var Avatar = React.createClass({
+    render : function(){
+        return (
+            <div>
+                <PagePic pagename={this.props.pagename}></PagePic>
+                <PageLink pagename={this.props.pagename}></PageLink>
+            </div>
+        )
+    }
+});
+
+ReactDOM.render(
+    <Avatar pagename='Machine Learning'></Avatar>,
+    document.getElementById('app')
+)
